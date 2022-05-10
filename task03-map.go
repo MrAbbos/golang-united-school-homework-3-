59a63f4a6f6917d528b56862585b4654ba665ad2
mirabbos2002 <69849085+mirabbos2002@users.noreply.github.com>
2022-05-10 10:08:21 +0500

    removed unnecessary prints

--- task03-map.go
+++ task03-map.go
@@ -1,12 +1,7 @@
 package homework
 
-import "fmt"
-
 func sortMapValues(input map[int]string) (result []string) {
-
-	fmt.Println(input)
 	for i := 0; i <= len(input); i++ {
-		fmt.Println(input[i], i)
 		if _, ok := input[i]; ok {
 			result = append(result, input[i])
 		}
